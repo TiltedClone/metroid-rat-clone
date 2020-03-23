ec0a70a93527a524fe12ff92a67256a53ed65158
MrMurly <gustav.melefors@gmail.com>
2020-03-23 10:49:37 +0100

    Update main.cpp

--- metroid-rat-clone/metroid-rat-clone/main.cpp
+++ metroid-rat-clone/metroid-rat-clone/main.cpp
@@ -1,9 +1,29 @@
 #include <SFML/Graphics.hpp>
-
+#include "sprite_animation.h"
 
 
 int main()
 {
-	sf::RenderWindow renderwindow;
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sprite_animation s(561, 144, 7, "C:/Users/gurra/OneDrive/Bilder/MegaMan.png");
+	
+	window.setFramerateLimit(16);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		s.update_animation();
+
+		window.draw(s);
+		window.display();
+	}
+
 	return 0;
 }
